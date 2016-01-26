cd94cf17d110c67cd1b937221cc6d905ab386387
calhopp <calhopp@gmail.com>
2016-01-26 11:19:32 -0600

    First Deploy

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
+
+#Things to do:
+#Front page with links to everywhere else
+#Contact page
+#Page with examples of my work
+#Page listing services/prices
+#Page listing future plans
+#Page listing why I'm doing this
